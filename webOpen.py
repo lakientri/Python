d5b9e5a6abcf4064447504e61025177afac94dc4
lenmorld <lenmorld@gmail.com>
2018-12-04 21:48:35 -0500

    get all elements, get their ids, loop through them to apply

--- webOpen.py
+++ webOpen.py
@@ -17,41 +17,65 @@ htmlSearch.send_keys("junior developer")
 htmlSearch.send_keys(Keys.ENTER)
 
 # get link element
-link = browser.find_element_by_id('sja1')
-# copy link address
-link_address = link.get_attribute("href")
+# link = browser.find_element_by_id('sja1')
 
-# go to link
-browser.get(link_address)
+# get all jobs in current page
+links = browser.find_elements_by_class_name('jobtitle')
+link_ids = []
 
-# second page now loaded in current browser
-# get job title
-job_title = browser.find_element_by_class_name("jobsearch-JobInfoHeader-title").text
-#salary = browser.find_element_by_class_name("jobsearch-JobMetadataHeader-item").text
+for index in range(len(links)):
+    link = links[index]
+    link_ids.append(link.get_attribute('id'))
 
-#apply= browser.find_element_by_id("indeedApplyButtonContainer").click()
-apply= browser.find_element_by_class_name("jobsearch-IndeedApplyButton")
-apply.click()
+for index in range(len(link_ids)):
+    # copy link address
+    link_id = link_ids[index]
+    link = browser.find_element_by_id(link_id)
+    link_address = link.get_attribute("href")
 
-#===========iFrame=========================================
-#time.sleep(15)
-#print("test", browser.find_element_by_class_name("indeed-apply-bd"))
-# netmail
+    # go to link
+    browser.get(link_address)
 
-# get the list of iframes present on the web page using tag "iframe"
-iframe = browser.find_elements_by_tag_name("iframe")
+    # get job info
+    job_title = browser.find_element_by_class_name("jobsearch-JobInfoHeader-title").text
+    # salary = browser.find_element_by_class_name("jobsearch-JobMetadataHeader-item").text
 
-# Switch to the frame[0]
-print("frame0 ",browser.switch_to_frame(iframe[0]))
+    print("Job title: ", job_title)
 
-# Need to go back to default content if want to go to ANOTHER FRAME,IF only 1 frame , do not need to  switch_to_default_content
-#browser.switch_to_default_content()
+    #apply= browser.find_element_by_id("indeedApplyButtonContainer").click()
+    apply= browser.find_element_by_class_name("jobsearch-IndeedApplyButton")
+    apply.click()
 
-#print("frame1 ",browser.switch_to_frame(iframe[1]))
-# go to "Second" iframe to look for src then open it to new browser
-src= browser.find_element_by_tag_name("iframe").get_attribute("src")
-getSrc= browser.get(src)
-browser.find_element_by_class_name("ia-BrowserDefaultFilePicker-control").click()
+    ### DO STUFF HERE TO APPLY FOR CURRENT JOB IN LOOP ###
+
+    #===========iFrame=========================================
+    #time.sleep(15)
+    #print("test", browser.find_element_by_class_name("indeed-apply-bd"))
+    # netmail
+
+    # get the list of iframes present on the web page using tag "iframe"
+    iframe = browser.find_elements_by_tag_name("iframe")
+
+    # Switch to the frame[0]
+    print("frame0 ",browser.switch_to_frame(iframe[0]))
+
+    # Need to go back to default content if want to go to ANOTHER FRAME,IF only 1 frame , do not need to  switch_to_default_content
+    #browser.switch_to_default_content()
+
+    #print("frame1 ",browser.switch_to_frame(iframe[1]))
+    # go to "Second" iframe to look for src then open it to new browser
+    src= browser.find_element_by_tag_name("iframe").get_attribute("src")
+    getSrc= browser.get(src)
+    upload_element = browser.find_element_by_class_name("ia-BrowserDefaultFilePicker-control")
+    # upload_element.click()
+    upload_element.send_keys("C:/resume_sample.pdf")
+
+    ### AFTER APPLYING FOR THIC CURRENT JOB, GO BACK TO JOB LIST 
+    ### TO MAKE SURE NEXT LINK IS OBTAINED
+
+    browser.execute_script("window.history.go(-1)")     # back one to get out of iframe
+    browser.execute_script("window.history.go(-1)")     # back one to get back to page list
+
 #=========================================================
 
 print("No of frames present in the web page are: ", len(iframe))
@@ -68,7 +92,3 @@ for index in range(len(seq)):
     browser.find_element_by_tag_name('a').send_keys(Keys.CONTROL, 'a')
     time.sleep(2)
 """
- 
-
-print("Job title: ", job_title)
-print("apply: ", apply)
